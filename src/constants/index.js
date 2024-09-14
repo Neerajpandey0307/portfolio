db105a8b56b7c6738809f6429d1194b4a8dd0974
Neerajpandey0307 <neeraj.pandey0307@gmail.com>
2024-09-14 14:03:09 +0530

    index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -158,7 +158,7 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      "I thought it was impossible to make a website as beautiful as our product.",
     name: "Vijay Chaudhary",
     designation: "Member & Investor",
     company: "Almora Cable Networks",
