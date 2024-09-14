ef8c53a400ae0415254010aeb51f16f74f7586a8
Neerajpandey0307 <neeraj.pandey0307@gmail.com>
2024-09-14 20:23:36 +0530

    Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -31,18 +32,23 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
+    // PPNIa1y1KuJFWhYjK
+// template_c8nosfe
+// service_267k8yq
+
+
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_267k8yq',
+        'template_c8nosfe',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Neeraj Pandey",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "neeraj.pandey0307@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'PPNIa1y1KuJFWhYjK'
       )
       .then(
         () => {
